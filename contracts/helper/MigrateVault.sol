d14a458d27c25a0fc2ddaa4aff34bc2b8eb60a7d
Nicolas <lapingarou@gmail.com>
2023-09-14 10:09:28 +0800

    cleanup: useless import

--- contracts/helper/MigrateVault.sol
+++ contracts/helper/MigrateVault.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import "../openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "../openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import "../GeVault.sol";
-import "../../interfaces/IWETH.sol";
 
 
 /// @notice Migrate liquidity from a vault to another vault
@@ -21,11 +20,11 @@ contract MigrateVault {
 
   /// @notice Migrate liquidity from an older vault
   function migrate(uint amount, address token, address payable sourceVault, address payable targetVault) public payable returns (uint liquidity){
-    if(amount == 0) amount = ERC20(sourceVault).balanceOf(msg.sender);
-    ERC20(sourceVault).transferFrom(msg.sender, address(this), amount);
+    if(amount == 0) amount = IERC20(sourceVault).balanceOf(msg.sender);
+    IERC20(sourceVault).transferFrom(msg.sender, address(this), amount);
     GeVault(sourceVault).withdraw(amount, token);
-    uint tAmount = ERC20(token).balanceOf(address(this));
-    ERC20(token).approve(targetVault, tAmount);
+    uint tAmount = IERC20(token).balanceOf(address(this));
+    IERC20(token).approve(targetVault, tAmount);
     if (address(this).balance > 0 && token == WETH )
       liquidity = GeVault(targetVault).deposit{value: address(this).balance}(token, 0);
     else
